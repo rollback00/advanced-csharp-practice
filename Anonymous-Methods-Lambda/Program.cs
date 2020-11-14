0e3b401ca0dd26485a07c320b8611adf12af4533
Michael Eshun <mykie.eshun@gmail.com>
2020-11-14 15:40:37 +0000

    Anonymous functions with lambda

---
+++ Anonymous-Methods-Lambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Anonymous_Methods_Lambda
+{
+  class Program
+  {
+
+    delegate void Operation(int num);
+
+    static void Main(string[] args)
+    {
+      //Operation op = delegate (int num) { Console.WriteLine($"{num} * {num} = {num * num}"); };
+      //Operation op = num => { Console.WriteLine($"{num} * {num} = {num * num}"); };
+      
+      Action<int> op = num => { Console.WriteLine($"{num} * {num} = {num * num}"); };
+      op(6);
+
+      Func<int, int> Double = num => { return num * 2; };
+
+      Console.WriteLine(Double(5));
+    }
+  }
+}
