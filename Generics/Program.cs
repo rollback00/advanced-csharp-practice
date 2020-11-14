d6ed1b4be802d519598893a01cc4a5728ce3b2ca
Michael Eshun <mykie.eshun@gmail.com>
2020-11-14 09:12:45 +0000

    generic class with one type

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using advanced_csharp.Generics;
+using System;
 
 namespace Generics
 {
@@ -6,7 +7,13 @@ namespace Generics
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      //Console.WriteLine("Hello World!");
+
+      var result1 = new Result<int> { Success = true, Data = 12 };
+      var result2 = new Result<string> { Success = true, Data = "John" };
+
+      Console.WriteLine($"Result 1 Data: {result1.Data.GetType()}");
+      Console.WriteLine($"Result 2 Data: {result2.Data.GetType()}");
     }
   }
 }
---
+++ Generics/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advanced_csharp.Generics
+{
+  class Result<T>
+  {
+    public bool Success { get; set; }
+    public T Data { get; set; }
+  }
+}
