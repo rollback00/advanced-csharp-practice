a14ec12e89c264161dab071164fbdcf93165fadb
Michael Eshun <mykie.eshun@gmail.com>
2020-11-14 08:39:17 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace advanced_csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      var firstName = "John";
+      var lastName = new StringBuilder();
+
+      
+    }
+  }
+}
